9cf947e4c85449301440f31af48586164f2c4439
tldnjs2213 <tldnjs2213@naver.com>
2022-05-20 23:17:18 +0900

    Updates

---
+++ precache-manifest.80c0096ad912afe695e173b1f3e74378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ce346a506c15616705a061c1d9d912",
+    "url": "/Ninth-Portfolio/index.html"
+  },
+  {
+    "revision": "32fe84332ca25e3c60ae",
+    "url": "/Ninth-Portfolio/static/css/main.dd66e9dc.chunk.css"
+  },
+  {
+    "revision": "293f2d0234843f59bdf0",
+    "url": "/Ninth-Portfolio/static/js/2.59b0d8aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ninth-Portfolio/static/js/2.59b0d8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fe84332ca25e3c60ae",
+    "url": "/Ninth-Portfolio/static/js/main.395128f6.chunk.js"
+  },
+  {
+    "revision": "2de5ec759b0065959f53",
+    "url": "/Ninth-Portfolio/static/js/runtime-main.3490d856.js"
+  }
+]);
